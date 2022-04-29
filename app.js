a4eb1f85ceef0c6844006c741cb29d126d08f6c8
robertolatini31 <robertolatini31@gmail.com>
2022-04-29 14:34:08 +0200

    added email list

--- app.js
+++ app.js
@@ -1,14 +1,30 @@
+// Attraverso l'apposita API di Boolean
+//  https://flynn.boolean.careers/exercises/api/random/mail 
+//  generare 10 indirizzi email e stamparli in 
+//  pagina all'interno di una lista.
+
+// Bonus
+// Far comparire gli indirizzi email solamente 
+// quando sono stati tutti generati.
+
+
 const app = new Vue({
     el: '#app',
     data: {
         emailGenerated: []
     },
     mounted() {
-        axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-        .then((email) => {
-            console.log(email);
-            console.log(email.data.response);
-        })
+
+        for(let i = 0; i < 10; i++){
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then((email) => {
+                //console.log(email);
+               console.log(i + ') ' + email.data.response);  
+                this.emailGenerated.push(email.data.response);
+            })   
+            
+        }
+       
     }
 
 })
